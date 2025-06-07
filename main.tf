2f4eae608f97610b1f13a22ea201e349c7f72fa9
adinarayanap <50868220+adinarayanap@users.noreply.github.com>
2025-06-07 14:44:23 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,18 @@
-#Starting with Terraform 
-#Add the basic code
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "<PROJECT_ID>"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
